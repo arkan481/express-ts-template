622b0cbb0b9a2930c5f1da32bda8ee2ea90ae5a9
arkan481 <arkanharyo@gmail.com>
2021-11-25 11:03:23 +0700

    server was setup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-// TODO: SETUP SERVER, CONSTANTS
+// TODO: CONSTANTS
 // TODO: SETUP LOGGER ON UNHANDLED REJECTION
 // TODO: SETUP LOGGER ON EVERY REQUEST USING MORGAN WITH EXPRESS APP
 import dotenv from 'dotenv';
+import express from 'express';
+import logger from './config/Logger';
 
 // Using .env
 dotenv.config();
+
+// instansiating express app
+const app = express();
+
+app.listen(process.env.PORT || 5000, () => {
+  logger.info(
+    `Server is running in ${process.env.NODE_ENV} mode, on port: ${process.env.PORT}`,
+  );
+});
