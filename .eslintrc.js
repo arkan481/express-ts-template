b546ce5bf5193426e01e6a6a144700a51675e61d
arkan481 <arkanharyo@gmail.com>
2021-11-25 10:44:10 +0700

    linter config was modified

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,35 @@
 module.exports = {
-	env: {
-		es2021: true,
-		node: true,
-	},
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaVersion: 13,
-		sourceType: 'module',
-	},
-	plugins: ['@typescript-eslint'],
-	rules: {
-		indent: ['error', 'tab'],
-		'linebreak-style': ['error', 'unix'],
-		quotes: ['error', 'single'],
-		semi: ['error', 'always'],
-		'@typescript-eslint/no-var-requires': 'error',
-		'no-unused-vars': 'off',
-		'@typescript-eslint/no-unused-vars': ['error'],
-	},
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-var-requires': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts'],
+      },
+    },
+  },
 };
