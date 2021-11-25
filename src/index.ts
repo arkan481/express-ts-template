3c271981627131b169f5dbd5c1b1454b231bf4a3
arkan481 <arkanharyo@gmail.com>
2021-11-25 11:15:15 +0700

    all requests are now logged

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
 // TODO: SETUP LOGGER ON EVERY REQUEST USING MORGAN WITH EXPRESS APP
 import dotenv from 'dotenv';
 import express from 'express';
+import morgan from 'morgan';
 import logger from './config/Logger';
 
 // Using .env
@@ -11,6 +12,19 @@ dotenv.config();
 // instansiating express app
 const app = express();
 
+// Using logger to log requests
+const loggerMode = process.env.NODE_ENV === 'production' ? 'combined' : 'dev';
+app.use(
+  morgan(loggerMode, {
+    stream: {
+      write: (info: String) => {
+        logger.info(info);
+      },
+    },
+  }),
+);
+
+// start express to listen to a port
 app.listen(process.env.PORT || 5000, () => {
   logger.info(
     `Server is running in ${process.env.NODE_ENV} mode, on port: ${process.env.PORT}`,
